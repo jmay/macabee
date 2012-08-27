83c59b51466705f9fd9bed7e71a8169f375759d4
Jason W. May <jmay@pobox.com>
2012-08-27 12:08:39 -0700

    macabee revise

--- lib/macabee/contacts.rb
+++ lib/macabee/contacts.rb
@@ -3,8 +3,8 @@
 class Macabee::Contacts
   attr_reader :ab
 
-  # suck all the contacts from local MacOSX Address Book into a single array
   def initialize
+    # Apple docs recommend using `addressBook`, not `sharedAddressBook`
     @ab = ABAddressBook.addressBook
   end
 
@@ -39,8 +39,9 @@ class Macabee::Contacts
     end
   end
 
+  # suck all the contacts from local MacOSX Address Book into a single array
   def contacts
-    @ab.people.map {|abperson| Macabee::Contact.new(abperson)}
+    @contacts ||= @ab.people.map {|abperson| Macabee::Contact.new(abperson)}
   end
 
   def groups
@@ -52,6 +53,12 @@ class Macabee::Contacts
       'contacts' => contacts.map(&:to_hash),
       'groups' => groups.map(&:to_hash)
     }
+  end
+
+  def contacts_indexed
+    @contacts_indexed ||= contacts.each_with_object({}) do |contact, hash|
+      hash[contact.uuid] = contact
+    end
   end
 
   def find(hash)
@@ -72,6 +79,40 @@ class Macabee::Contacts
     contact.compare(hash)
   end
 
+  def diffs(contactlist)
+    contactlist.each_with_object({}) do |data, changes|
+      if data['xref'] && data['xref']['ab'] != 'DELETED'
+        # Any record that doesn't have an AB identifier is new, so should be ignore here
+        # when looking for AB changes that need to go back.
+        # If the identifier says 'DELETED' then we don't want to look for it in AB; we might
+        # want to retain locally-deleted entries in our source, but we don't want to try to
+        # reconstitute them in AB.
+
+        contact = find(data)
+        if contact
+          changeset = contact.compare(data)
+          if changeset.any?
+            # there have been changes
+            changes[contact.uuid] = changeset
+          end
+          # if there are no changes, emit nothing for this record
+        else
+          # This contact does not appear in the target, assume it has been deleted.
+          # Construct a changeset that will reflect that back onto the source data.
+          old_uid = data['xref']['ab']
+          changes[old_uid] = [
+            [
+              '~',
+              'xref.ab',
+              'DELETED',
+              data['xref']['ab']
+            ]
+          ]
+        end
+      end
+    end
+  end
+
   def apply(hash)
     contact = find(hash)
     diffs = contact.compare(hash)
@@ -83,4 +124,15 @@ class Macabee::Contacts
   def save!
     @ab.save
   end
+
+  def additions(contactlist)
+    newkeys = contacts_indexed.keys - contactlist.map{|c| c['xref'] && c['xref']['ab']}
+    newkeys.each_with_object({}) {|k, hash| hash[k] = contacts_indexed[k].to_hash}
+  end
+
+  # collection of record changes describing AB data state that doesn't match the inbound source records
+  def revise(contactlist)
+    additions(contactlist).merge(diffs(contactlist))
+  end
+
 end
